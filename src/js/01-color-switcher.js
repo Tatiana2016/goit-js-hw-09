f58b19129ef42add2cef3f586b42cf58043f6f92
Tatiana2016 <serarfima@gmail.com>
2023-04-16 19:26:12 +0300

    01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+
+let intervalId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+startButton.addEventListener('click', () => {
+  // Перевіряємо, чи не запущений вже таймер
+  if (intervalId === null) {
+    // Запускаємо таймер і зберігаємо його ідентифікатор
+    intervalId = window.setInterval(() => {
+      const newColor = getRandomHexColor();
+      document.body.style.backgroundColor = newColor;
+    }, 1000);
+    // Вимикаємо кнопку "Start"
+    startButton.disabled = true;
+  }
+});
+
+stopButton.addEventListener('click', () => {
+  // Перевіряємо, чи запущений таймер
+  if (intervalId !== null) {
+    // Зупиняємо таймер і очищаємо його ідентифікатор
+    window.clearInterval(intervalId);
+    intervalId = null;
+    // Увімкнути кнопку "Start"
+    startButton.disabled = false;
+  }
+});
+console.log(getRandomHexColor);
 
